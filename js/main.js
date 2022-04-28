cbd5e0271d3e5df89accf3fd1ec4396fb4732444
Fabio Moro <fabio.moro2000@gmail.com>
2022-04-28 17:27:24 +0200

    terza versione

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
-let discount = false;
-const age = prompt("quanti anni hai?");
-const km = prompt("quanti kilometri vuoi percorrere?");
+const N = prompt("di quanti biglietti vuoi calcolare il prezzo?")
 
-firstPrice = 0.21*km;
+for (let i=0; i < N; i++) {
+    let discount = false;
+    const age = prompt("quanti anni ha il viaggiatore numero "+(i+1)+"?");
+    const km = prompt("quanti kilometri vuole percorrere?");
 
-if(age < 18) {
-    finalPrice = firstPrice - (20*firstPrice)/100;
-    discount = true;
+    firstPrice = 0.21*km;
+
+    if(age < 18) {
+        finalPrice = firstPrice - (20*firstPrice)/100;
+        discount = true;
+    }
+
+    else if (age >= 65) {
+        finalPrice = firstPrice - (40*firstPrice)/100;
+        discount = true;
+    }
+
+    else {
+        finalPrice = firstPrice;
+    }
+
+    rounded = finalPrice.toFixed(2);
+
+    document.getElementById("container").innerHTML += "biglietto numero: "+ i + "<br>";
+
+    if (discount) {
+        document.getElementById("container").innerHTML +="prezzo iniziale: " + firstPrice + "$" + "<br>";
+        document.getElementById("container").innerHTML +="prezzo scontato: " + rounded + "$" + "<br><br>";
+    }
+
+    else {
+        document.getElementById("container").innerHTML +="prezzo biglietto: " + firstPrice + "$" + "<br><br>";
+    }
 }
 
-else if (age >= 65) {
-    finalPrice = firstPrice - (40*firstPrice)/100;
-    discount = true;
-}
 
-else {
-    finalPrice = firstPrice;
-}
 
-rounded = finalPrice.toFixed(2);
 
-if (discount) {
-    document.getElementById("price").innerHTML ="prezzo iniziale: " + firstPrice + "$";
-    document.getElementById("price-d").innerHTML ="prezzo scontato: " + rounded + "$";
-}
-
-else {
-    document.getElementById("price").innerHTML ="prezzo biglietto: " + firstPrice + "$";
-}
